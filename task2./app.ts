8454eac5976ab6ffba2f9f5e8afc8508dbcd02e6
Akbota Bagdauletkyzy <akbotabagdauletkyzy@MacBook-Akbota.local>
2026-02-19 14:44:09 +0500

    1

--- task2./app.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-import {User} from './user';
-
-@Component({
-  selector: 'app-root',
-  template: ` <app-user name ="Serdar" /> `,
-  imports: [User],
-})
-export class App {}
